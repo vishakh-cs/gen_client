69c7b63832fba926f10364444ebd1611cfb20c08
vishakh-cs <143394085+vishakh-cs@users.noreply.github.com>
2024-04-19 19:24:48 +0530

    clearence

--- generative/app/not-found.tsx
+++ generative/app/not-found.tsx
@@ -90,7 +90,7 @@ const router = useRouter()
           <span className="text-gray-500 text-6xl block">
             <span>4 0 4</span>
           </span>
-          <span className="text-gray-500 text-xl">Sorry, We couldn't find what you are looking for!</span>
+          <span className="text-gray-500 text-xl">Sorry, We could not find what you are looking for!</span>
         </div>
         <div className="mt-6">
           <div
